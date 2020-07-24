51395c237aac702b8bf66d3929679c0868370313
Leonel Rodriguez <leonellrodriguez@gmail.com>
2020-07-23 20:35:18 -0500

    first commit 2

--- index.js
+++ index.js
@@ -14,15 +14,14 @@ app.use(cors());
 // Habilitar express.json
 app.use(express.json({extend: true}));
 
-// Puerro de la app
-const port = process.env.port || 4000;
-
 // Importar rutas
 app.use('/api/usuario', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
+// Puerro de la app
+const port = process.env.port || 4000;
 // Arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
