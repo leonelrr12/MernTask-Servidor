4f3ac97f008907ddd2f90fa5ce0ef7b29cc23dcf
Leonel Rodriguez <leonellrodriguez@gmail.com>
2020-07-23 19:32:26 -0500

    Ajustes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({extend: true}));
 
 // Puerro de la app
-const PORT = process.env.port || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use('/api/usuario', require('./routes/usuarios'));
